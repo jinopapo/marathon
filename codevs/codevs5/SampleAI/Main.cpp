da4d08539909b3830dc6042acc74189e49434d28
nezawa atsuhiro <mfm.ellegarden@gmail.com>
2016-03-06 19:22:59 +0900

    add .gitignore

--- codevs/codevs5/SampleAI/Main.cpp
+++ codevs/codevs5/SampleAI/Main.cpp
@@ -3,153 +3,10 @@
 #include <queue>
 #include <algorithm>
 #include <climits>
+#include <stack>
+#include "Status.h"
+
 using namespace std;
-
-const int INF = 876765346;
-
-const char CELL_EMPTY = '_';
-const char CELL_WALL ='W';
-const char CELL_OBJECT = 'O';
-
-class Point {
-public:
-  int x, y;
-  Point() { x = y = -1; }
-  Point(int x, int y): x(x), y(y) {}
-  bool operator== (const Point &p) const { return x == p.x && y == p.y; }
-};
-
-class Skill {
-public:
-  int id, cost;
-  Skill() { id = cost = -1; }
-  Skill(int id, int cost): id(id), cost(cost) {}
-
-  static Skill input(int id) {
-    int cost;
-    cin >> cost;
-    return Skill(id, cost);
-  }
-};
-
-class Cell : public Point {
-public:
-  char kind;
-  bool containsNinja, containsSoul, containsDog;
-  Cell() {
-    kind = '?';
-    containsNinja = containsSoul = containsDog = false;
-  }
-  Cell(int x, int y, char kind): Point(x, y), kind(kind) {
-    containsNinja = containsSoul = containsDog = false;
-  }
-
-  bool isWall() const { return kind == CELL_WALL; }
-  bool isObject() const { return kind == CELL_OBJECT; }
-  bool isEmpty() const { return kind == CELL_EMPTY; }
-};
-
-class Character : public Point {
-public:
-  int id;
-  Character() { id = -1; }
-  Character(int id, int x, int y): id(id), Point(x, y) {}
-
-  static Character input() {
-    int id, x, y;
-    cin >> id >> y >> x;
-    return Character(id, x, y);
-  }
-};
-
-class State {
-public:
-  int skillPoint;
-  int H, W;
-  vector< vector<Cell> > field;
-  vector<Character> ninjas;
-  vector<Character> dogs;
-  vector<Point> souls;
-  vector<int> skillCount;
-
-  State() {
-    skillPoint = H = W = -1;
-    field.clear();
-    ninjas.clear();
-    dogs.clear();
-    souls.clear();
-    skillCount.clear();
-  }
-
-  static State input(int numOfSkills) {
-    State st;
-
-    cin >> st.skillPoint;
-    cin >> st.H >> st.W;
-
-    st.field.clear();
-
-    for (int i = 0; i < st.H; i++) {
-      vector<Cell> line;
-      string s;
-      cin >> s;
-      for (int j = 0; j < s.size(); j++) {
-        line.push_back(Cell(j, i, s[j]));
-      }
-      st.field.push_back(line);
-    }
-
-    int numOfNinjas;
-    cin >> numOfNinjas;
-    st.ninjas.clear();
-    for (int i = 0; i < numOfNinjas; i++) {
-      Character ninja = Character::input();
-      st.ninjas.push_back(ninja);
-      st.field[ninja.y][ninja.x].containsNinja = true;
-    }
-
-    int numOfDogs;
-    cin >> numOfDogs;
-    st.dogs.clear();
-    for (int i = 0; i < numOfDogs; i++) {
-      Character dog = Character::input();
-      st.dogs.push_back(dog);
-      st.field[dog.y][dog.x].containsDog = true;
-    }
-
-    int numOfSouls;
-    cin >> numOfSouls;
-    st.souls = vector<Point>();
-    for (int i = 0; i < numOfSouls; i++) {
-      int x, y;
-      cin >> y >> x;
-      st.souls.push_back(Point(x, y));
-      st.field[y][x].containsSoul = true;
-    }
-
-    st.skillCount = vector<int>();
-    for (int i = 0; i < numOfSkills; i++) {
-      int count;
-      cin >> count;
-      st.skillCount.push_back(count);
-    }
-
-    return st;
-  }
-};
-
-class Search : public Point {
-public:
-  int dist;
-  Search(){}
-  Search(int x, int y, int dist): Point(x, y), dist(dist) {}
-};
-
-class MinDist {
-public:
-  int dogDist, soulDist;
-  MinDist(int dogDist, int soulDist): dogDist(dogDist), soulDist(soulDist) {}
-};
 
 int remTime;
 vector<Skill> skills;
@@ -159,6 +16,26 @@ State rivalState;
 int dx[] =    {  0,   1,   0,  -1,   0};
 int dy[] =    { -1,   0,   1,   0,   0};
 string ds[] = {"U", "R", "D", "L",  ""};
+
+bool isMoveStone(const State &st,int sx,int sy,int dir){
+  int nx = sx+dx[dir];
+  int ny = sy+dy[dir];
+  if(st.field[sy][sx].isWall()) return false;
+  return !st.field[ny][nx].isObj();
+}
+
+void SearchNear(nextState &st,int sx,int sy){
+  st.nearDogs = false;
+  st.free = 0;
+  for(int dir=0;dir<5;dir++){
+    int nx = sx + dx[dir];
+    int ny = sy + dy[dir];
+    if(dir != 5 && (st.state.field[ny][nx].isEmpty() || isMoveStone(st.state,nx,ny,dir)))
+      st.free++;
+    if(st.state.field[ny][nx].containsDog)
+      st.nearDogs = true;
+  }
+}
 
 /**
  * (sx, sy) を起点として、幅優先探索で (最も近い忍犬までの距離, 最も近いアイテムまでの距離) を計算して MinDist で返します。
@@ -214,24 +91,26 @@ MinDist getMinDist(const State &st, int sx, int sy) {
  * - 移動先にニンジャソウルが存在する場合、取得処理(忍力回復する, フィールドのソウルフラグをfalseにする, 取得済みのソウルの座標削除)が行われます。
  * (※簡単なシミュレートのため、壁を押すなどの処理は行われません)
  */
-void simulateWalk(int id, int dir) {
-  int nx = myState.ninjas[id].x + dx[dir];
-  int ny = myState.ninjas[id].y + dy[dir];
-  if (!myState.field[ny][nx].isEmpty()) return;
+void simulateWalk(int id, int dir,State &st) {
+  int nx = st.ninjas[id].x + dx[dir];
+  int ny = st.ninjas[id].y + dy[dir];
+  if (!st.field[ny][nx].isEmpty() && !isMoveStone(st,nx,ny,dir)) return;
 
-  myState.ninjas[id].x = nx;
-  myState.ninjas[id].y = ny;
+  st.ninjas[id].x = nx;
+  st.ninjas[id].y = ny;
 
-  if (!myState.field[ny][nx].containsSoul) return;
+  if (!st.field[ny][nx].containsSoul) return;
 
   // 忍力回復
-  myState.skillPoint += 2;
+  st.skillPoint += 2;
 
   // フィールドのフラグをfalseに
-  myState.field[ny][nx].containsSoul = false;
+  st.field[ny][nx].containsSoul = false;
+  st.field[ny][nx].kind = CELL_OBJECT;
+  st.field[st.ninjas[id].y][st.ninjas[id].x].kind = CELL_EMPTY;
 
   // 取得済みのソウルの座標削除
-  myState.souls.erase( find(myState.souls.begin(), myState.souls.end(), Point(nx, ny)) );
+  //st.souls.erase( find(myState.souls.begin(), myState.souls.end(), Point(nx, ny)) );
 }
 
 /*
@@ -242,53 +121,67 @@ void simulateWalk(int id, int dir) {
  * - 自分自身のマスから連結であるマスの中にニンジャソウルが存在しない場合は、忍犬までの最短距離が最大になるように移動します。
  * -- 忍犬も存在しない場合は、その場にとどまります。
  */
-string thinkByNinjaId(int id) {
-  vector<MinDist> dists;
-  vector<Point> points;
+void thinkByNinjaId(int id,int playCount) {
+  vector<string> dirs;
+  int maxFree=0;
+  vector<int> freeDir;
+  stack<nextState> dfs;
+  vector<nextState> nexts;
+  nextState nState;
+  nState.state = myState;
+  nState.free = 0;
+  nState.nearDogs = false;
+  dfs.push(nState);
 
-  for (int dir = 0; dir < 5; dir++) {
-    int nx = myState.ninjas[id].x + dx[dir];
-    int ny = myState.ninjas[id].y + dy[dir];
-    dists.push_back(getMinDist(myState, nx, ny));
-    points.push_back(Point(nx, ny));
-  }
-
-  // ニンジャソウルへ近づく方向
-  int minDistSoul = INF;
-  int minDistSoulDir = 4;
-
-  // 忍犬から遠ざかる方向
-  int maxDistDog = -INF;
-  int maxDistDogDir = 4;
-
-  for (int dir = 0; dir < 5; dir++) {
-    // この方向には歩くことができないので無視
-    if (dists[dir].soulDist == -INF) continue;
-
-    // 忍犬までの距離が1以下になってしまうので無視
-    if (dists[dir].dogDist <= 1) continue;
-
-    if (minDistSoul > dists[dir].soulDist) {
-      minDistSoul = dists[dir].soulDist;
-      minDistSoulDir = dir;
+  //このターン全探索
+  while(!dfs.empty()){
+    nState = dfs.top();
+    dfs.pop();
+    if(maxFree <= nState.free){
+      bool contain = false;
+      for(nextState ns:nexts){
+        if(ns.dir == nState.dir)
+          contain = true;
+      }
+      if(!contain && !nState.nearDogs){
+        if(maxFree < nState.free && maxFree < 3){
+          maxFree = min(nState.free,3);
+          nexts.clear();
+        }
+        nexts.push_back(nState);
+      }
     }
-
-    if (maxDistDog < dists[dir].dogDist) {
-      maxDistDog = dists[dir].dogDist;
-      maxDistDogDir = dir;
+    if((int)nState.dir.size() != playCount){
+      for(int dir=0;dir<4;dir++){
+        int nx = nState.state.ninjas[id].x + dx[dir];
+        int ny = nState.state.ninjas[id].y + dy[dir];
+        if(!nState.state.field[ny][nx].isWall()){
+          if(nState.state.field[ny][nx].isEmpty() || isMoveStone(nState.state,nx,ny,dir)){
+            nextState ns =  nState;
+            simulateWalk(id,dir,ns.state);
+            ns.dir.push_back(ds[dir]);
+            SearchNear(ns,nx,ny);
+            dfs.push(ns);
+          }
+        }
+      }
+      cerr << endl;
     }
   }
 
-  int dir = 4;
-  if (minDistSoul != INF) {
-    dir = minDistSoulDir;
-  } else {
-    dir = maxDistDogDir;
+  if(nexts.size() != 0){
+    int maxSoul = 0;
+    vector<nextState> nss;
+    for(auto ns:nexts){
+      if(maxSoul <= ns.state.skillPoint){
+        nss.push_back(ns);
+        maxSoul = ns.state.skillPoint;
+      }
+    }
+    nState = nss[rand()%nss.size()];
+    myState = nState.state;
+    for(string s:nState.dir) cout << s;
   }
-
-  if (dir == 4) return ds[dir];
-  simulateWalk(id, dir);
-  return ds[dir];
 }
 
 /*
@@ -311,10 +204,8 @@ void think() {
     cout << 2 << endl;
   }
 
-  for (int i = 0; i < myState.ninjas.size(); i++) {
-    for (int j = 0; j < moveLoop; j++) {
-      cout << thinkByNinjaId(myState.ninjas[i].id);
-    }
+  for (int i = 0; i < (int)myState.ninjas.size(); i++) {
+    thinkByNinjaId(myState.ninjas[i].id,moveLoop);
     cout << endl;
   }
 }
@@ -337,8 +228,7 @@ bool input() {
 }
 
 int main() {
-  // AIの名前を出力
-  cout << "SampleAI.cpp" << endl;
+  cout << "ji_no_papo" << endl;
   cout.flush();
 
   while (input()) {
---
+++ codevs/codevs5/SampleAI/Status.h
@@ -0,0 +1,163 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+const int INF = 876765346;
+
+const char CELL_EMPTY = '_';
+const char CELL_WALL ='W';
+const char CELL_OBJECT = 'O';
+
+class Point {
+public:
+  int x, y;
+  Point() { x = y = -1; }
+  Point(int x, int y): x(x), y(y) {}
+  bool operator== (const Point &p) const { return x == p.x && y == p.y; }
+};
+
+class Skill {
+public:
+  int id, cost;
+  Skill() { id = cost = -1; }
+  Skill(int id, int cost): id(id), cost(cost) {}
+
+  static Skill input(int id) {
+    int cost;
+    cin >> cost;
+    return Skill(id, cost);
+  }
+};
+
+class Cell : public Point {
+public:
+  char kind;
+  bool containsNinja, containsSoul, containsDog;
+  Cell() {
+    kind = '?';
+    containsNinja = containsSoul = containsDog = false;
+  }
+  Cell(int x, int y, char kind): Point(x, y), kind(kind) {
+    containsNinja = containsSoul = containsDog = false;
+  }
+
+  bool isWall() const { return kind == CELL_WALL; }
+  bool isObject() const { return kind == CELL_OBJECT; }
+  bool isEmpty() const { return kind == CELL_EMPTY; }
+  bool isObj() const {
+    return containsDog || containsNinja || isWall() || isObject();
+  }
+};
+
+class Character : public Point {
+public:
+  int id;
+  Character() { id = -1; }
+  Character(int id, int x, int y): id(id), Point(x, y) {}
+
+  static Character input() {
+    int id, x, y;
+    cin >> id >> y >> x;
+    return Character(id, x, y);
+  }
+};
+
+class State {
+public:
+  int skillPoint;
+  int H, W;
+  vector< vector<Cell> > field;
+  vector<Character> ninjas;
+  vector<Character> dogs;
+  vector<Point> souls;
+  vector<int> skillCount;
+
+  State() {
+    skillPoint = H = W = -1;
+    field.clear();
+    ninjas.clear();
+    dogs.clear();
+    souls.clear();
+    skillCount.clear();
+  }
+
+  static State input(int numOfSkills) {
+    State st;
+
+    cin >> st.skillPoint;
+    cin >> st.H >> st.W;
+
+    st.field.clear();
+
+    for (int i = 0; i < st.H; i++) {
+      vector<Cell> line;
+      string s;
+      cin >> s;
+      for (int j = 0; j < s.size(); j++) {
+        line.push_back(Cell(j, i, s[j]));
+      }
+      st.field.push_back(line);
+    }
+
+    int numOfNinjas;
+    cin >> numOfNinjas;
+    st.ninjas.clear();
+    for (int i = 0; i < numOfNinjas; i++) {
+      Character ninja = Character::input();
+      st.ninjas.push_back(ninja);
+      st.field[ninja.y][ninja.x].containsNinja = true;
+    }
+
+    int numOfDogs;
+    cin >> numOfDogs;
+    st.dogs.clear();
+    for (int i = 0; i < numOfDogs; i++) {
+      Character dog = Character::input();
+      st.dogs.push_back(dog);
+      st.field[dog.y][dog.x].containsDog = true;
+    }
+
+    int numOfSouls;
+    cin >> numOfSouls;
+    st.souls = vector<Point>();
+    for (int i = 0; i < numOfSouls; i++) {
+      int x, y;
+      cin >> y >> x;
+      st.souls.push_back(Point(x, y));
+      st.field[y][x].containsSoul = true;
+    }
+
+    st.skillCount = vector<int>();
+    for (int i = 0; i < numOfSkills; i++) {
+      int count;
+      cin >> count;
+      st.skillCount.push_back(count);
+    }
+
+    return st;
+  }
+};
+
+class Search : public Point {
+public:
+  int dist;
+  Search(){}
+  Search(int x, int y, int dist): Point(x, y), dist(dist) {}
+};
+
+class MinDist {
+public:
+  int dogDist, soulDist;
+  MinDist(int dogDist, int soulDist): dogDist(dogDist), soulDist(soulDist) {}
+};
+
+struct nextState{
+  vector<string> dir;
+  int free;
+  bool nearDogs;
+  State state;
+};
